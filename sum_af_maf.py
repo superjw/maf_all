eb3733c65bd661279cc92730b999439a1202aa28
superxiaopobao <jw.wang@live.com>
2016-05-18 18:24:27 +0100

    add af to each 19.19.rewrite.script.mapping.tsv file, the output file is

--- sum_af_maf.py
+++ sum_af_maf.py
@@ -143,7 +143,7 @@ def main():
     # exmaple usage: python3 sum_af_maf.py 22 1000
     chromosome = sys.argv[1]
     # chromosome = '22'
-    flank = sys.argv[2]o
+    flank = sys.argv[2]
     # flank = 1000
     infile = str(chromosome) + 'mapped_snp_af_1k_flank_.tsv'
     # the infile is the mapping result file, generated from the zcat | tt3.py
