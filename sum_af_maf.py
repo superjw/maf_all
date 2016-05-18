19ddb3e42377a84b32084de25a1938558962208e
superxiaopobao <jw.wang@live.com>
2016-05-18 18:19:01 +0100

    fixed the af problem, add af_for_all into the script, which can result the final output contains the AF.

--- sum_af_maf.py
+++ sum_af_maf.py
@@ -2,8 +2,7 @@
 """
 this is a rewrite version of maf calculation script(08/Apr/2016)
 # exmaple usage: python3 sum_af_maf.py 22 1000
-# output file: \*\.gid\.maf\.af\.tsv
-
+# output file: '\*\.gid\.maf\.af\.tsv'
 # this file is modified from the file sum_af_maf.py in order to calculate
 # af for all the samples. The input files are \*mapped_snps_af_1k_flank_.tsv (19/May/2016)
 
@@ -23,8 +22,10 @@ def gene_length_dict(chromosome, flank):
     :param flank: flank length
     :return: dictionary {gene, length}
     """
-    file_name = os.path.abspath('/home/nis/jingwei/work/1k_analysis/gene_location/pos.chr' +
-                                str(chromosome) + '.gene.tsv')
+    # file_name = os.path.abspath('/home/nis/jingwei/work/1k_analysis/gene_location/pos.chr' +
+    #                             str(chromosome) + '.gene.tsv')
+    file_name = './pos.chr' + str(chromosome) + '.gene.tsv'
+    # the above line is used for local test
     d = {}
     with open(file_name, 'r') as f:
         for line in f:
@@ -59,7 +60,9 @@ def set_of_gene(infile):
     all_gene_id = filter(None, set(all_gene_id))
     # use the filer function to filter out the None results generated when no ENSG exists in the line
     f.close()
-    # print(chrome + 'list building finished!')
+    # for a in all_gene_id:
+    #     print(a, end='\t')
+    # print('list building finished!')
     return all_gene_id
 
 
@@ -70,6 +73,7 @@ def deal_biallelic(l):
     :return:list, list in the same order(biallelic sites have been summed up)
     """
     bia = []
+    # print(l)
     for a in l:
         # print(a)
         the_sum = Decimal(0)
@@ -80,6 +84,7 @@ def deal_biallelic(l):
                 # bia.append(the_sum)
         else:
             the_sum += Decimal(a)
+            # print(type(the_sum))
             # print(type(a))
         bia.append(the_sum)
     return bia
@@ -89,18 +94,33 @@ def maf(gene_id, infile_name):
     """
    check if gene in the line, and sum up the maf
     :param gene_id: ensemble gene_id
-    :param infile_name: mapped file with MAF in, generated from zcat | tt3.py
+    :param infile_name: mapped file with MAF/af in, generated from zcat | tt3.py
     :return: decimal, maf of each superpopulation
     """
     with open(infile_name, 'r') as f:
-        eas = amr = afr = eur = sas = Decimal(0)
-        af = Decimal(0)
+        eas = amr = afr = eur = sas = af = Decimal(0)
         # i = Decimal(0)
         for line in f:
             if gene_id in line:
+                # print(gene_id, end='\t')
+                # print(line)
                 line_lst = line.strip().split('\t')
-                af = line[10]
-                new_maf = deal_biallelic(line_lst[3:8].append(af))
+                # print(line_lst[3:8])
+                # print('line_lst[10l' + line_lst[10])
+                # print(line_lst[8])
+                af_all = line_lst[10].strip()
+                # print('af', af)
+                # print('aflist')
+                # print(type(Decimal(af)))
+                l = line_lst[3:8]
+                l.append(af_all)
+                # for a in l:
+                #     print(a, end='\t')
+                #     print('@@@@@@@@')
+                # print(l)
+                # print(type(line_lst[3:8]))
+                # print('-----------')
+                new_maf = deal_biallelic(l)
                 eas += new_maf[0]
                 amr += new_maf[1]
                 afr += new_maf[2]
@@ -121,8 +141,10 @@ def maf(gene_id, infile_name):
 
 def main():
     # exmaple usage: python3 sum_af_maf.py 22 1000
-    chromosome = sys.argv[1]
-    flank = sys.argv[2]
+    # chromosome = sys.argv[1]
+    chromosome = '22'
+    # flank = sys.argv[2]o
+    flank = 1000
     infile = str(chromosome) + 'mapped_snp_af_1k_flank_.tsv'
     # the infile is the mapping result file, generated from the zcat | tt3.py
     outf = open(str(chromosome) + '.gid.maf.af.tsv', 'w')
@@ -133,6 +155,10 @@ def main():
     length_dict = gene_length_dict(chromosome, flank)
     for gene_id in gene_list:
         length = Decimal(length_dict.get(gene_id))
+        # print(str(gene_id) + '\t' + str(length))
+        # print('=======')
+        # print(str(length))
+        # print('^^^^^^^^^^^')
         eas, amr, afr, eur, sas, af = maf(gene_id, infile)
         avg_eas = (eas / length).quantize(Decimal('0.00001'))
         avg_amr = (amr / length).quantize(Decimal('0.00001'))
@@ -140,12 +166,6 @@ def main():
         avg_eur = (eur / length).quantize(Decimal('0.00001'))
         avg_sas = (sas / length).quantize(Decimal('0.00001'))
         avg_af = (af / length).quantize(Decimal('0.00001'))
-        # print(gene_id + '\t' +
-        #       str(avg_eas) + '\t' +
-        #       str(avg_amr) + '\t' +
-        #       str(avg_afr) + '\t' +
-        #       str(avg_eur) + '\t' +
-        #       str(avg_sas))
         outf.write(gene_id + '\t' +
                    str(avg_eas) + '\t' +
                    str(avg_amr) + '\t' +
@@ -158,3 +178,19 @@ def main():
 
 
 main()
+# length_dict = gene_length_dict(22, 1000)
+# for x, y in length_dict.items():
+#     print(x + ':' + str(y))
+#  dictionary works fine
+
+# infile = str(22) + 'mapped_snp_af_1k_flank_.tsv'
+# # gene_list = set_of_gene(infile)
+# # for x in gene_list:
+# #     print(x)
+# # gene_list works fine
+# eas, amr, afr, eur, sas, af = maf('ENSG00000233866', infile)
+# # print(eas)
+# # print(af)
+# print('@@@')
+
+
